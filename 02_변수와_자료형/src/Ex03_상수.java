6d3d6d868e612c26744826a761ace8fe696d0347
ParkJaeHan <mitshowme@naver.com>
2024-09-09 13:15:37 +0900

    fist commit

---
+++ 02_변수와_자료형/src/Ex03_상수.java
@@ -0,0 +1,16 @@
+public class Ex03_상수 {
+    public static void main(String[]args){
+        int age = 20;
+        System.out.println(age);
+
+        age =21;
+        System.out.println(age);
+
+        final double PI = 3.14;
+        System.out.println(PI);
+
+        // PI = 3.14159;
+
+        // System.out.println(PI);
+    }
+}
---
+++ 02_변수와_자료형/src/Ex04_문자열.java
@@ -0,0 +1,16 @@
+public class Ex04_문자열 {
+    public static void main(String[] args) {
+        String name;
+        name = "자바학생";
+        System.out.println(name);
+
+        String empty = "";
+        System.out.println(empty);
+
+        String space = "   ";
+        System.out.println(space);
+
+        String nvalue = null;
+        System.out.println(nvalue);
+    }
+}
